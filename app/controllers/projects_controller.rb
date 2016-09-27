2ef7a20ec9770952225f85bc25f0e5a6dffeff43
Nomi Vos <nvoswork@gmail.com>
2016-09-27 16:22:13 -0500

    initial set up for projects controller, routes and views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,26 @@
 class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all.order('created_at desc')
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new project_params
+
+    if @project.save
+      redirect_to @project, notice; 'Nice, Nomi!  That project was successfully saved!'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :link)
+  end
 end
