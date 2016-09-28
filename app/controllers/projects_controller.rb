130635bc13eb0d6acc006759049624d119968926
Nomi Vos <nvoswork@gmail.com>
2016-09-28 04:50:25 -0500

    sets up projects new, show and index pages, controllers and views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
 
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+  
   def index
     @projects = Project.all.order('created_at desc')
   end
@@ -12,13 +14,21 @@ class ProjectsController < ApplicationController
     @project = Project.new project_params
 
     if @project.save
-      redirect_to @project, notice; 'Nice, Nomi!  That project was successfully saved!'
+      redirect_to @project, notice: 'Nice, Nomi!  That project was successfully saved!'
     else
       render 'new'
     end
   end
 
+  def show
+
+  end
+
   private
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
 
   def project_params
     params.require(:project).permit(:title, :description, :link)
