256faaf75b8d58e0c2fda6aa153f60bf35c182f5
Nomi Vos <nvoswork@gmail.com>
2016-09-28 05:01:13 -0500

    sets up friendly_id for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
 
   before_action :find_project, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @projects = Project.all.order('created_at desc')
   end
@@ -27,10 +27,10 @@ class ProjectsController < ApplicationController
   private
 
   def find_project
-    @project = Project.find(params[:id])
+    @project = Project.friendly.find(params[:id])
   end
 
   def project_params
-    params.require(:project).permit(:title, :description, :link)
+    params.require(:project).permit(:title, :description, :link, :slug)
   end
 end
