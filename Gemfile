5422ecd348126fcdb70c16d0753b22d1e50a6e7f
Nomi Vos <nvoswork@gmail.com>
2016-09-30 06:16:14 -0500

    remove code from development that was causing an error, also remove dotenv gem

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ ruby '2.3.1'
 
 gem 'font-awesome-rails'
 gem 'rails', '4.2.7.1'
-gem 'dotenv-rails'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  has_attached_file :image, styles: { large: "600x600", medium: "300x300>", thumb: "150x150>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150>", square: '200x200#' }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
