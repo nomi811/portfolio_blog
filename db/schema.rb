bb8011ee2af78feeb9c5d1474b78fc8bbcfeb76e
Nomi Vos <nvoswork@gmail.com>
2016-10-10 16:55:04 -0500

    beginning of set up for photo page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010212147) do
+ActiveRecord::Schema.define(version: 20161010215007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20161010212147) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image_id"
   end
 
   create_table "posts", force: :cascade do |t|
