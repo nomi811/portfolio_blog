47978af930fc8afbc48e016c3be8a7fb643bd618
Nomi Vos <nvoswork@gmail.com>
2016-10-13 11:54:32 -0500

    sets up pattern controller like photo controller

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -26,28 +26,20 @@ class PatternsController < ApplicationController
   def create
     @pattern = Pattern.new(pattern_params)
 
-    respond_to do |format|
-      if @pattern.save
-        format.html { redirect_to @pattern, notice: 'Pattern was successfully created.' }
-        format.json { render :show, status: :created, location: @pattern }
-      else
-        format.html { render :new }
-        format.json { render json: @pattern.errors, status: :unprocessable_entity }
-      end
+    if @pattern.save
+      redirect_to @pattern, notice: 'Pattern was successfully created.'
+    else
+      render :new, notice: 'Unable to save pattern.'
     end
   end
 
   # PATCH/PUT /patterns/1
   # PATCH/PUT /patterns/1.json
   def update
-    respond_to do |format|
-      if @pattern.update(pattern_params)
-        format.html { redirect_to @pattern, notice: 'Pattern was successfully updated.' }
-        format.json { render :show, status: :ok, location: @pattern }
-      else
-        format.html { render :edit }
-        format.json { render json: @pattern.errors, status: :unprocessable_entity }
-      end
+    if @pattern.update(pattern_params)
+      redirect_to @pattern, notice: 'Pattern was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -55,20 +47,18 @@ class PatternsController < ApplicationController
   # DELETE /patterns/1.json
   def destroy
     @pattern.destroy
-    respond_to do |format|
-      format.html { redirect_to patterns_url, notice: 'Pattern was successfully destroyed.' }
-      format.json { head :no_content }
+      redirect_to patterns_url, notice: 'Pattern was successfully destroyed.'
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pattern
-      @pattern = Pattern.find(params[:id])
+      @pattern = Pattern.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pattern_params
-      params.require(:pattern).permit(:name)
+      params.require(:pattern).permit(:name, :slug, :image, :remove_image)
     end
 end
