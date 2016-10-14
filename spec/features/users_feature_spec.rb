6958b5ff60a3161b24981ba23d6d39d347475ae6
Nomi Vos <nvoswork@gmail.com>
2016-10-14 18:38:29 -0500

    test passing for ability for me to sign up

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -2,8 +2,16 @@ feature 'User access' do
   context 'user not signed in and on index page' do
     it 'can not see "Sign in or Sign out" links' do
       visit '/'
-      expect(page).not_to have_link 'Sign out'
-      expect(page).not_to have_link 'Sign in'
+      expect(page).not_to have_link 'Sign Out'
+      expect(page).not_to have_link 'Sign In'
+    end
+  end
+
+  context 'I can' do
+    it 'sign up' do
+      sign_up
+      expect(current_path).to eq '/'
+      expect(page).to have_content 'Welcome! You have signed up successfully'
     end
   end
 end
