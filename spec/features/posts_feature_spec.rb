5d0e00a26635687f539c60e886d35325821cfaa9
Nomi Vos <nvoswork@gmail.com>
2016-10-14 18:29:24 -0500

    sets up factory girl. test passing for creating a post and showing on individual page

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,14 +1,16 @@
 feature 'Creating posts' do
-
-  scenario 'correct page route actions' do
+  background do
     sign_up
     visit '/posts/new'
+    post_one = create(:post, title: 'This is post one')
+    post_two = create(:post, title: 'This is the second post')
+  end
+
+  scenario 'correct page route actions' do
     expect(page).to have_content('New Post')
   end
 
   scenario 'can create a post' do
-    sign_up
-    visit '/posts/new'
     attach_file('Image', "spec/files/images/heron-600px.jpg")
     fill_in 'Title', with: 'Heron'
     fill_in 'Write your article here', with: 'some content'
@@ -17,13 +19,23 @@ feature 'Creating posts' do
     expect(page).to have_css("img[src*='heron-600px.jpg']")
   end
 
-  it 'does not need an image to create a post' do
-    sign_up
-    visit '/posts/new'
+  scenario 'does not need an image to create a post' do
     fill_in 'Title', with: 'Example Post'
     fill_in 'Write your article here', with: 'some content'
     click_button 'Create Post'
     expect(page).not_to have_css("img[scr*='missing.png']")
+  end
+
+  scenario 'the index displays correct created posts information' do
+    visit '/posts'
+    expect(page).to have_content('This is post one')
+    expect(page).to have_content('This is the second post')
+  end
+
+  scenario 'can view individual posts' do
+    post = create(:post)
+    click_button 'Create Post'
+    expect(page).to have_content 'Right on, Nomi! Your article was successfully saved!'
   end
 
 end
