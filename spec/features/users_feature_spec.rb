3db3f841443cfc6cea99c6e05b2b57fda6059430
Nomi Vos <nvoswork@gmail.com>
2016-10-14 13:27:28 -0500

    tests for no sign in or sign out links on welcome page for a visitor

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -1,8 +1,9 @@
 feature 'User access' do
   context 'user not signed in and on index page' do
-    it 'can not see "Sign in" link' do
+    it 'can not see "Sign in or Sign out" links' do
       visit '/'
       expect(page).not_to have_link 'Sign out'
+      expect(page).not_to have_link 'Sign in'
     end
   end
 end
