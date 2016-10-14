fbe10335bb40a219b7bd4e8ec7e7ad0f7335d541
Nomi Vos <nvoswork@gmail.com>
2016-10-14 13:24:09 -0500

    passing test for no image placeholder when image is not added to post

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,12 +1,29 @@
 feature 'Creating posts' do
+
+  scenario 'correct page route actions' do
+    sign_up
+    visit '/posts/new'
+    expect(page).to have_content('New Post')
+  end
+
   scenario 'can create a post' do
     sign_up
     visit '/posts/new'
-    # attach_file('image', "spec/files/images/heron-600px.jpg")
-    # fill_in 'Title', with: 'Heron'
-    # fill_in 'Write your article here', with: 'some content'
-    # click_button 'Create Post'
-    expect(page).to have_content('New Post')
-    # expect(page).to have_css("img[src*='heron-600px.jpg']")
+    attach_file('Image', "spec/files/images/heron-600px.jpg")
+    fill_in 'Title', with: 'Heron'
+    fill_in 'Write your article here', with: 'some content'
+    click_button 'Create Post'
+    expect(page).to have_content('Heron')
+    expect(page).to have_css("img[src*='heron-600px.jpg']")
   end
+
+  it 'does not need an image to create a post' do
+    sign_up
+    visit '/posts/new'
+    fill_in 'Title', with: 'Example Post'
+    fill_in 'Write your article here', with: 'some content'
+    click_button 'Create Post'
+    expect(page).not_to have_css("img[scr*='missing.png']")
+  end
+
 end
