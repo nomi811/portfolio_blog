322ab5eccf175d1538ad05709e343a0deb274fe0
Nomi Vos <nvoswork@gmail.com>
2016-10-20 11:34:16 -0500

    rubocob led me astray! removes tick marks around rgex so tests pass again.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,5 +7,5 @@ class Photo < ActiveRecord::Base # :nodoc:
                                       thumb: '150x150>',
                                       square: '200x200#' },
                             default_url: '/images/:style/missing.png'
-  validates_attachment_content_type :image, content_type: `/\Aimage\/.*\z/`
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
