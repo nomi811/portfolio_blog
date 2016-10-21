090d99d90c1a09c89b5d99fd9453b73f8419d301
Nomi Vos <nvoswork@gmail.com>
2016-10-21 06:19:47 -0500

    tests for validations on user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,27 @@
 describe User do
-  it 'is authenticable' do
-    user = User.create(
+  subject {
+    described_class.new(
       email: 'test@example.com',
       password: 'password',
       password_confirmation: 'password'
     )
-    expect(user.valid_password?('password')).to be_truthy
+  }
+  context 'Authentication' do
+    it 'is authenticable' do
+      expect(subject.valid_password?('password')).to be_truthy
+    end
+  end
+  context 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+    it 'is not valid without a password' do
+      subject.password = nil
+      expect(subject).not_to be_valid
+    end
+    it 'is not valid without an email' do
+      subject.email = nil
+      expect(subject).not_to be_valid
+    end
   end
 end
