f5a19df202e9e2c3411949b7aeacf7b13399122a
Nomi Vos <nvoswork@gmail.com>
2016-10-25 13:37:50 -0500

    changes layout of posts on landing page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController # :nodoc:
   def index
-    @posts = Post.all.limit(3).order('created_at desc')
-    @projects = Project.all.limit(3).order('created_at desc')
+    @posts = Post.all.limit(4).order('created_at desc')
+    @projects = Project.all.limit(4).order('created_at desc')
   end
 end
