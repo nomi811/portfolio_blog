1c8a2dc84e26d6d8d28c5a74634ed0f7dc096658
Nomi Vos <nvoswork@gmail.com>
2016-10-25 15:35:27 -0500

    sets up photos and patterns on the landing page. Changes routing

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,7 @@ class WelcomeController < ApplicationController # :nodoc:
   def index
     @posts = Post.all.limit(4).order('created_at desc')
     @projects = Project.all.limit(4).order('created_at desc')
+    @photos = Photo.all.limit(4)
+    @patterns = Pattern.all.limit(4)
   end
 end
