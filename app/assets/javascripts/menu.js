59d5970647b9fea760a9b8c007ca94a65b6004d0
Nomi Vos <nvoswork@gmail.com>
2016-11-10 15:30:31 -0600

    trying to fix the menu drop down

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $("#burger-nav").on("click", function() {
-    $("#main_navigation nav ul").toggleClass("open");
+    $("#menu nav ul").toggleClass("open");
   });
 });
