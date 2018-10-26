873ddb96f0e912dda0ecb3259163fd19c0770d95
Nomi Vos <nvoswork@gmail.com>
2018-10-26 14:38:29 -0500

    remove unecessary routes to patterns, photos, projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :patterns
-  resources :photos
-  resources :photos
   get 'about/index'
 
   devise_for :users
   resources :posts
-  resources :projects
-  resources :pattern_design
   resources :contacts, only: %i[new create]
   get 'welcome/index'
   root 'welcome#index'
