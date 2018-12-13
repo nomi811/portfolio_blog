fcec56993f4f3cb5cbc45b5084c0b93aa7a17d61
Nomi Vos <nvoswork@gmail.com>
2018-12-13 10:32:14 -0600

    removes images references in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,7 @@ class Post < ActiveRecord::Base # :nodoc:
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  has_attached_file :image, styles: { large: '600x600>',
-                                      medium: '300x300>',
-                                      thumb: '150x150>',
-                                      square: '200x200#' },
-                            default_url: '/images/:style/missing.png'
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  
 
   validates_presence_of :title, :content
 end
